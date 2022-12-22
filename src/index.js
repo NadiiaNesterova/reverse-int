cabf0314f58a4cb10184e63bcaa14d8948448d95
Nadiia Nesterova <nesterova1717@gmail.com>
2022-12-22 18:40:19 +0200

    feat: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let reverseNum = 0; 
+    
+    while (n > 0) {
+      reverseNum = reverseNum * 10 + n % 10;
+      n = Math.floor(n / 10);
+    }
+    return reverseNum;
 }
